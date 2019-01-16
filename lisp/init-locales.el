45316ebe6d343b2e64503a1799bc4946a77179f3
josephyoung <2t@live.com>
2019-01-16 15:03:41 +0800

    change back

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -18,7 +18,7 @@
   (setq locale-coding-system 'utf-8)
   (set-default-coding-systems 'utf-8)
   (set-terminal-coding-system 'utf-8)
-  (set-selection-coding-system 'utf-8);; (if (eq system-type 'windows-nt) 'utf-16-le
+  (set-selection-coding-system (if (eq system-type 'windows-nt) 'utf-16-le 'utf-8))
 
   (prefer-coding-system 'utf-8))
 
